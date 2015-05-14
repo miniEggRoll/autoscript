651007f99fbd406bd04481b1dd6ddc6fe6980f34
mrsquid0126@gmail.com <mrsquid0126@gmail.com>
2015-05-14 18:34:00 +0800

    image

--- jobs/restaurantPhoto/start.sh
+++ jobs/restaurantPhoto/start.sh
@@ -1,5 +1,4 @@
 curl -o images.zip http://storage.googleapis.com/eztable-static/restaurant/images.zip
-unzip images.zip
 docker run -w="/app" -e NODE_ENV=dev -v `pwd`:/app node:0.12.2-wheezy \
     bash -c "apt-get update && apt-get install unzip && unzip images.zip && npm install && npm start"
-rm -rf images
+sudo rm -rf images
